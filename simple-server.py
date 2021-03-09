a202a0a70ff34301ea4e46282185fd0fff4cbabb
ElusiveFox <44260690+ElusiveFoxie@users.noreply.github.com>
2021-03-09 15:03:02 +0100

    Update simple-server.py

--- simple-server.py
+++ simple-server.py
@@ -15,21 +15,28 @@ simple-server 1.0 by ElusiveFox
 usage: simple-server.py [-m] [-p]
 
 -m, --mode 	http or https (default: http)
--p, --port  	port number
+-p, --port  	port number (default: 8080)
 
 example: simple-server.py https 8443
 
 '''
 
+PORT = 8080
+MODE = 'http'
+
 
 if __name__ == '__main__':
 	try:
-		PORT = int(sys.argv[2])
-		MODE = sys.argv[1]
+		if (len(sys.argv) == 3):
+			PORT = int(sys.argv[2])
+			MODE = sys.argv[1]
+		if (len(sys.argv) == 2):
+			PORT = int(sys.argv[1])
+
 		Handler = http.server.SimpleHTTPRequestHandler
 
 		with socketserver.TCPServer(("", PORT), Handler) as httpd:
-			if(sys.argv[1]=="https"):
+			if(MODE=="https"):
 				httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
 			print("serving "+MODE+" at port", PORT)
 			httpd.serve_forever()
